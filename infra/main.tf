5f32cf089ffe46c5283e15c953acada1bc433758
Quantum Developer <quantum.dev@example.com>
2025-04-15 17:14:37 +0530

    Adding tf recovery and eks access configuration

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "terraform-quantum-microservice-state-bucket"
+    key            = "eks/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    # dynamodb_table = "terraform-locks"
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
@@ -9,6 +26,22 @@ module "eks" {
   subnet_ids         = ["subnet-0536a460c909e7014", "subnet-0b4c5332592f763c0"]  # Use your subnet IDs
   vpc_id          = "vpc-0b43409b967dc118f"                        # Your VPC ID
 
+  # Add this block
+  access_config = {
+    endpoint_public_access  = true
+    endpoint_private_access = true
+  }
+
+  manage_aws_auth_configmap = true
+
+  aws_auth_users = [
+    {
+      userarn  = "arn:aws:iam::816069124994:user/admin"
+      username = "admin"
+      groups   = ["system:masters"]
+    }
+  ]
+  
   eks_managed_node_groups = {
     default = {
       desired_capacity = 1
