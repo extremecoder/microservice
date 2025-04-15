507bbee45662b36b1ce782bc1f14f7c7f25f8f39
Quantum Developer <quantum.dev@example.com>
2025-04-15 14:16:23 +0530

    Fix typo in EKS module configuration by changing 'subnets' to 'subnet_ids' in main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -6,7 +6,7 @@ module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = "eks1"
   cluster_version = "1.28"
-  subnets         = ["subnet-0536a460c909e7014", "subnet-0b4c5332592f763c0"]  # Use your subnet IDs
+  subnet_ids         = ["subnet-0536a460c909e7014", "subnet-0b4c5332592f763c0"]  # Use your subnet IDs
   vpc_id          = "vpc-0b43409b967dc118f"                        # Your VPC ID
 
   eks_managed_node_groups = {
